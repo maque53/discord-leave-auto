8d97fd38bed13d02e63c6640604d0f6d3b4ffad1
maque <dexfbackmaque@gmail.com>
2025-10-16 20:25:15 -0300

    Create auto-leave.js

---
+++ src/auto-leave.js
@@ -0,0 +1,13 @@
+
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client();
+const chalk = require('chalk');
+
+client.on('ready', async () => {
+  console.log(chalk.black.bgGreen('[ + ]') + chalk.green.underline(` Auto-leave ativo para ${client.user.tag}`));
+  
+  const { processGuilds } = require('../backend/leave.js');
+  await processGuilds(client);
+});
+
+client.login(process.env.TOKEN || 'TOKEN);
